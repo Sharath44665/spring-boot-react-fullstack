8cfb86451e02eb6cc2b3808083959bc48803ad70
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-11-26 21:10:41 +0530

    hello world

---
+++ myapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+    
+    <h1>Hello world</h1>
+    <p>this is my first React Component</p>
+    </div>
+  );
+}
+
+export default App
