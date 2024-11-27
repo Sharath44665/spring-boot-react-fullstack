e5a453ec30e7394ac94dc9368938ba536c750317
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-11-27 14:28:27 +0530

    add user data

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,19 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+
 function App() {
   // const [count, setCount] = useState(0)
- 
-  const myArr = ['apple', 'banana', 'orange']
-  const myList = myArr.map((item) => <p>{item}</p>);
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
   
   return (
     <>
+    <h1>{user.name}</h1>
+    <img className='avatar' src={user.imageUrl} 
+      style={{width:user.imageSize, height: user.imageSize}}/>
 
-    
-    {myList}
-
-    
     </>
   );
 }
--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,23 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App'
+import App from './App'
 
-class MyApp extends React.Component{
-    onInputChange(event){
-        console.log(event.target.value);
-    }
-
-    render() {
-        return (
-            <div>
-                <form>
-                    <label htmlFor="">Enter Text</label>
-                    <input type='text' onChange={this.onInputChange}/>
-                </form>
-            </div>
-        );
-    }
-}
 
 
 // ReactDOM.render(<MyApp/>, document.querySelector('#root')); // depricated
@@ -25,4 +9,4 @@ const container = document.getElementById('root');
 
 const root = ReactDOM.createRoot(container);
 
-root.render(<MyApp/>)
+root.render(<App/>)
