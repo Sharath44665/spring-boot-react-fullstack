2d5855fc8e97c56be2e0e9e9441bfbec694f2c2e
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-11-27 11:35:51 +0530

    modules

---
+++ myapp/react_es6/04messageExport.js
@@ -0,0 +1,7 @@
+const message = () => {
+    const name = "Sharath";
+    const age = 22;
+    return `${name} is ${age} years old`;
+}
+
+export default message;
---
+++ myapp/react_es6/04printMsgImport.js
@@ -0,0 +1,2 @@
+import msg from './04messageExport.js';
+console.log(msg()) // Sharath is 22 years old
